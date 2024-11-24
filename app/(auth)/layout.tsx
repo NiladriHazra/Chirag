33d52a4afe0f07f83362b833b1aca8ae3d908bc0
Niladri Hazra <151611097+NiladriHazra@users.noreply.github.com>
2024-11-24 15:41:52 +0530

    added logo

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -22,7 +22,7 @@ const Layout = ({children}: {children: React.ReactNode}) => {
         <section className="flex flex-1 flex-col items-center bg-white p-4 py-10 lg:justify-center lg:p-10 lg:py-0">
         <div className="mb-16 lg:hidden">
           <Image
-            src="/Logo1.png"
+            src="/Logo.png"
             alt="logo"
             width={224}
             height={82}
